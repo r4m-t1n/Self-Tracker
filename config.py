b01451654655f9c9fd5f0b50cdd1e3e17a451a4f
r4m-t1n <imramtinkhodaei@gmail.com>
2025-05-22 23:34:28 +0330

    Add config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# Database
+HOST = "localhost"
+DATABASE = "self_tracker"
+USER = "postgres"
+PASSWORD = ""
+PORT = 5432
+
+# Icons folder
+FOLDER = "icons\\"
+
+# Icons ---- MessageBox
+ERROR = FOLDER + "error.png"  # add .png at the end of the filename
+WARNING = FOLDER + "warning.png"
+SUCCESS = FOLDER + "success.png"
+
+# Icons ---- Main windows
+W_MAIN = FOLDER + "self_tracker.png"
+W_HABITS = FOLDER + "window_habits.png"
+W_TASKS = FOLDER + "window_tasks.png"
+W_RECORD = FOLDER + "window_record.png"
+W_PROGRESS = FOLDER + "window_progress.png"
+
+# Icons ---- others
+ADD = FOLDER + "add.png"
+RECORD = FOLDER + "record.png"
+START = FOLDER + "start.png"
+STOP = FOLDER + "stop.png"
+PAUSE = FOLDER + "pause.png"
